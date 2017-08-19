882c217a225482f46353880833a3116856a67591
Austin C <ac6404@gmail.com>
2017-08-19 10:16:50 -0500

    Added markers and sorting functionality to list view.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,111 @@
-// document.getElementById('test').addEventListener('click', function() {
-//           geocodeAddress("2199 California Ave, St. Louis, MO");
-//         });
-
+//**************************** Data Model ************************************
 var place = function(name, address){
-  this.name = ko.observable(name);
-  this.address = ko.observable(address);
-  this.latlong = {lat:0.0, lng:0.0};
+  this.name = name;
+  this.address = address;
+  this.latlong = {lat:0.0, lng:0.0};``
+  this.id = 0;
+  this.info = ko.observable("You have not clicked me...");
+  this.showInfo = false;
 };
 
-var viewModel = {
-  places: [
-    new place("Lona's Lil Eats", "2199 California Ave., St. Louis, MO"),
-    new place("Fox Park", "2716 Shenandoah Ave., St. Louis, MO"),
-    new place("Trader Bob's Tattoo Shop", "2529 Jefferson Avenue, St. Louis, MO"),
-    new place("The Purple Martin", "2800 Shenandoah Ave, St. Louis, MO"),
-    new place("DeSales Community Garden", "2635 California Ave, St. Louis, MO"),
-    new place("Keypers Piano Bar", "2280 Jefferson Avenue, St. Louis, MO"),
-    new place("Fritanga","2208 Jefferson Avenue, St. Louis, MO"),
-    new place("Southside Early Childhood Center", "2101 S Jefferson Ave, St. Louis, MO"),
-    new place("7-Eleven", "2607 Gravois Ave, St. Louis, MO"),
-    new place("Dutch Town Auto Brokerage","2737 Gravois Ave, St. Louis, MO"),
-    new place("Concrete Ocean Art Gallery", "2257 S Jefferson Ave, St. Louis, MO")
-  ]
+var places = [
+new place("Lona's Lil Eats", "2199 California Ave., St. Louis, MO"),
+new place("Fox Park", "2716 Shenandoah Ave., St. Louis, MO"),
+new place("Trader Bob's Tattoo Shop", "2529 Jefferson Avenue, St. Louis, MO"),
+new place("The Purple Martin", "2800 Shenandoah Ave, St. Louis, MO"),
+new place("DeSales Community Garden", "2635 California Ave, St. Louis, MO"),
+new place("Keypers Piano Bar", "2280 Jefferson Avenue, St. Louis, MO"),
+new place("Fritanga","2208 Jefferson Avenue, St. Louis, MO"),
+new place("Southside Early Childhood Center", "2101 S Jefferson Ave, St. Louis, MO"),
+new place("7-Eleven", "2607 Gravois Ave, St. Louis, MO"),
+new place("Dutch Town Auto Brokerage","2737 Gravois Ave, St. Louis, MO"),
+new place("Concrete Ocean Art Gallery", "2257 S Jefferson Ave, St. Louis, MO")
+];
 
-};
-
-function getCoord(){
-  for(i=0; i<viewModel.places.length; i++){
-    var address = viewModel.places[i].address();
+//**************************Geocode Places ************************************
+getCoord = function(){
+  for(i=0; i<places.length; i++){
+    var address = places[i].address;
     geocodeAddress(address, i);
   }
-}
+};
 
-function setCoord(resultlat, resultlng, i){
-  viewModel.places[i].latlong.lat = resultlat;
-  viewModel.places[i].latlong.lng = resultlng;
-
-}
+setCoord = function(resultlat, resultlng, i){
+  places[i].latlong.lat = resultlat;
+  places[i].latlong.lng = resultlng;
+  console.log("setCoord test: " + resultlat + "," + resultlng);
+};
 
 getCoord();
 
-console.log(JSON.stringify(viewModel.places));
+//***********************View Model******************************************
+var viewModel =  {
+  //create an observable array of places
+  placeList: ko.observableArray([]),
+
+  //adds all places to the list
+  //set id's for each place
+  initList: function(){
+    var i = 0;
+    places.forEach(function(place){
+      place.id = i;
+      viewModel.placeList.push(place);
+      i++;
+    });
+  },
+
+                      //********search filter***********//
+
+  //this watches user input text
+  filterText: ko.observable(""),
+
+  //called everytime text input changes in the filter box.
+  //it gets passed value, which is the content of the input box
+  filter: function(value){
+    //clear the list
+    viewModel.placeList.removeAll();
+    //clear map of markers
+    markers.forEach(function(marker){
+      marker.setVisible(false);
+    });
+
+    //stores place id for identifying marker
+    var id;
+
+    //add places back to the list if queries match
+    places.forEach(function(place){
+
+      //indexOf returns -1 if there is no match
+      if(place.name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        //addd place to viewModel
+        viewModel.placeList.push(place);
+
+        //grab place id and use it to display marker
+        id = place.id;
+        markers[id].setVisible(true);
+      }
+    });
+  },//end viewModel.filter
+
+              //********** List Click ***********//
+showInfo: function(place){
+  //first clear any open list info
+  viewModel.placeList().forEach(function(p){
+    p.info("You have not clicked me....");
+  });
+
+  place.info("You Clicked!");
+  //open marker info window for selected place
+  fillWindow(markers[place.id], infoWindow);
+}
+
+};
+
+//************************** Initialization Calls *********************************
+
+//set list to default state
+viewModel.initList();
+//this calls filter function when filterText is changed (any time user enters text)
+viewModel.filterText.subscribe(viewModel.filter);
+//apply data bindings
 ko.applyBindings(viewModel);
--- js/map.js
+++ js/map.js
@@ -1,12 +1,15 @@
 var map;
-
+var markers = [];
+var infoWindow;
 
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244},
-    zoom: 13
+    center: {lat: 38.607523, lng: -90.225898},
+    zoom: 16
   });
+
+  infoWindow = new google.maps.InfoWindow();
 
   // document.getElementById('test').addEventListener('click', function() {
   //           geocodeAddress("2199 California Ave, St. Louis, MO");
@@ -23,13 +26,46 @@ function geocodeAddress(address, i){
     if (status == google.maps.GeocoderStatus.OK) {
       resultlat = results[0].geometry.location.lat();
       resultlng = results[0].geometry.location.lng();
-    } else {
-      resultlat = -999;
-      resultlng = -999;
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
+      //send coordinates to our places array
+      setCoord(resultlat, resultlng, i);
 
-    console.log("geocode req: " + resultlat + ", " + resultlng);
-    setCoord(resultlat, resultlng, i);
+      //create a marker
+      createMarker(i);
+
+    } else {
+      alert('Geocode was not successful: ' + status);
+    }
+    console.log(address + ": " + resultlat + ", " + resultlng);
   });
+}
+
+function createMarker(id){
+  //create a marker and push to markers array
+  var marker = new google.maps.Marker({
+    map: map,
+    position: places[id].latlong,
+    title: places[id].name,
+    animation: google.maps.Animation.DROP,
+    id: id
+  });
+
+  markers.push(marker);
+  markers.sort(function(a, b){return a-b});
+
+  //add an infoWindow to the marker
+  marker.addListener('click', function(){
+    fillWindow(this, infoWindow);
+    //alert list view of selection
+    var selectedPlace = viewModel.placeList()[marker.id];
+    viewModel.showInfo(selectedPlace);
+  });
+}
+
+function fillWindow(marker, window) {
+  if (window.marker != marker) {
+    window.marker = marker;
+    window.setContent('<div>' + marker.title + '</div>');
+    window.open(map, marker);
+
+  }
 }
